b006c092be37bc6b641a13040dfa9b15af47e450
Saharariel <saharariel@gmail.com>
2018-10-17 17:28:26 +0300

    Add files via upload

---
+++ Modules/gil.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot.Modules
+{
+    public class gil : ModuleBase<SocketCommandContext>
+    {
+        
+        //[Command("join")]
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.Message.Author as IGuildUser)?.VoiceChannel;
+            if (channel == null) { await Context.Message.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            var audioClient = await channel.ConnectAsync();
+
+            await SendAsync(audioClient, "audio/hello.mp3");
+        }
+
+        private Process CreateStream(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg.exe",
+                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            });
+        }
+
+        private async Task SendAsync(IAudioClient client, string path)
+        {
+            // Create FFmpeg using the previous example
+            using (var ffmpeg = CreateStream(path))
+            using (var output = ffmpeg.StandardOutput.BaseStream)
+            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
+            {
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
+        }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+	class Program
+    { 
+
+		static void Main(string[] args)
+		=> new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+		{
+			_client = new DiscordSocketClient();
+
+			await _client.LoginAsync(TokenType.Bot, "NDc3MTk4Mjk1MzU4NTA0OTg0.Dn6gDg.2Ais4povJuFazKfOIfXREt-0KJA");
+
+			await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            Console.WriteLine("The bot is running !");
+
+            await Task.Delay(-1);
+
+        }
+
+        public static async Task Log(string message, LogSeverity severity = LogSeverity.Info, [System.Runtime.CompilerServices.CallerMemberName] string source = "", Exception exception = null)
+        {
+            await Console.Error.WriteLineAsync($"[{severity}][{source}] {message}");
+            if (exception != null)
+            {
+                await Console.Error.WriteLineAsync(exception.ToString());
+                await Console.Error.WriteLineAsync();
+            }
+        }
+    }
+}
